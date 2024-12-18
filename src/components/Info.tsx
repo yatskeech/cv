032b0c732e185c2b0d3335d77681e23b5e811369
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-18 21:59:26 +0300

    Reduce array of info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -9,23 +9,14 @@ interface Info {
 
 const INFO: Info[] = [
   {
-    icon: <IoIosMail className="text-xl" />,
+    icon: <IoIosMail />,
     title: "Email",
-    element: (
-      <a
-        href="mailto:dima.ja06@gmail.com"
-        className="text-gray-lighter leading-none"
-      >
-        dima.ja06@gmail.com
-      </a>
-    ),
+    element: <a href="mailto:dima.ja06@gmail.com">dima.ja06@gmail.com</a>,
   },
   {
-    icon: <IoMdPin className="text-xl" />,
+    icon: <IoMdPin />,
     title: "Address",
-    element: (
-      <span className="text-gray-lighter leading-none">Minsk, Belarus</span>
-    ),
+    element: <span>Minsk, Belarus</span>,
   },
 ];
 
@@ -34,10 +25,10 @@ function Info() {
     <div className="flex flex-col gap-6">
       {INFO.map(({ icon, title, element }, index) => (
         <div key={index} className="flex gap-4 items-center">
-          <div className="w-10 h-10 flex justify-center items-center bg-gray-darker rounded-full">
+          <div className="w-10 h-10 flex justify-center items-center bg-gray-darker rounded-full text-xl">
             {icon}
           </div>
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1 text-gray-lighter leading-none">
             <span className="text-xs text-gray-light leading-none">
               {title}
             </span>
