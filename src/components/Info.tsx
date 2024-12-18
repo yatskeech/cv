189b938a11a00d93d993c35d6f5fac98192b3ffe
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-18 21:57:19 +0300

    Add array of info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,31 +1,50 @@
+import { ReactNode } from "react";
 import { IoIosMail, IoMdPin } from "react-icons/io";
+
+interface Info {
+  icon: ReactNode;
+  title: string;
+  element: ReactNode;
+}
+
+const INFO: Info[] = [
+  {
+    icon: <IoIosMail className="text-xl" />,
+    title: "Email",
+    element: (
+      <a
+        href="mailto:dima.ja06@gmail.com"
+        className="text-gray-lighter leading-none"
+      >
+        dima.ja06@gmail.com
+      </a>
+    ),
+  },
+  {
+    icon: <IoMdPin className="text-xl" />,
+    title: "Address",
+    element: (
+      <span className="text-gray-lighter leading-none">Minsk, Belarus</span>
+    ),
+  },
+];
 
 function Info() {
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex gap-4 items-center">
-        <div className="w-10 h-10 flex justify-center items-center bg-gray-darker rounded-full">
-          <IoIosMail className="text-xl" />
+      {INFO.map(({ icon, title, element }, index) => (
+        <div key={index} className="flex gap-4 items-center">
+          <div className="w-10 h-10 flex justify-center items-center bg-gray-darker rounded-full">
+            {icon}
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="text-xs text-gray-light leading-none">
+              {title}
+            </span>
+            {element}
+          </div>
         </div>
-        <div className="flex flex-col gap-1">
-          <span className="text-xs text-gray-light leading-none">Email</span>
-          <a
-            href="mailto:dima.ja06@gmail.com"
-            className="text-gray-lighter leading-none"
-          >
-            dima.ja06@gmail.com
-          </a>
-        </div>
-      </div>
-      <div className="flex gap-4 items-center">
-        <div className="w-10 h-10 flex justify-center items-center bg-gray-darker rounded-full">
-          <IoMdPin className="text-xl" />
-        </div>
-        <div className="flex flex-col gap-1">
-          <span className="text-xs text-gray-light leading-none">Address</span>
-          <span className="text-gray-lighter leading-none">Minsk, Belarus</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
