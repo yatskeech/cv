7d754e8716b29da7931f42c9468748b477fcb1b3
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-20 16:04:57 +0300

    Fix header

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <div className="w-full min-h-screen bg-gray-darker">
       <div className="container">
         <div className="flex flex-col lg:flex-row items-center lg:items-stretch gap-4 text-white">
-          <div className="lg:min-w-[360px] min-h-screen bg-primary-darkest">
+          <div className="lg:min-w-[360px] lg:min-h-screen bg-primary-darkest">
             <div className="flex flex-col gap-8 p-8">
               <Header />
               <Divider />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-1 leading-none">
-        <h1 className="text-3xl font-bold">Dmitry Yastkevich</h1>
+        <h1 className="text-3xl font-bold">Dmitry Yatskevich</h1>
         <h2 className="text-xl text-gradient-primary">Frontend Developer</h2>
       </div>
       <div className="quote flex flex-col gap-1 leading-none">
